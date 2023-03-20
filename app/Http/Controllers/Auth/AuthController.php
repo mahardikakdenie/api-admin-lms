cb113dacc5ba52aa6b3f16d39760a9235be51f7d
MahardiikaKD <dikamahar884@gmail.com>
2023-03-20 10:25:32 +0700

    auth reguster and login admin

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -5,87 +5,120 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Brryfrmnn\Transformers\Json;
+
 
 class AuthController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function register(Request $request)
-    {
-        try {
-            $user = new User();
-            $user->name = $request->name;
-        } catch (\Throwable $th) {
-            //throw $th;
-        }
-    }
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function register(Request $request)
+	{
+		try {
+			$user = new User();
+			$user->name = $request->name;
+			$user->email = $request->email;
+			$user->password = Hash::make($request->password);
+			$user->save();
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+			return Json::response($user);
+		} catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
+			return Json::exception('Error Model ' . $debug = env('APP_DEBUG', false) == true ? $e : '');
+		} catch (\Illuminate\Database\QueryException $e) {
+			return Json::exception('Error Query ' . $debug = env('APP_DEBUG', false) == true ? $e : '');
+		} catch (\ErrorException $e) {
+			return Json::exception('Error Exception ' . $debug = env('APP_DEBUG', false) == true ? e : '');
+		}
+	}
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function login(Request $request)
+	{
+		try {
+			$user = User::where('name', $request->name)
+				->orWhere('email', $request->name)
+				->first();
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
+			if (!$user || !Hash::check($request->password, $user->password)) {
+				return Json::exception("Password Salah");
+			}
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
+			$token = [
+				'token' => $user->createToken('auth')->plainTextToken,
+				"user" => $user,
+			];
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
+			return Json::response($token);
+		} catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
+			return Json::exception('Error Model ' . $debug = env('APP_DEBUG', false) == true ? $e : '');
+		} catch (\Illuminate\Database\QueryException $e) {
+			return Json::exception('Error Query ' . $debug = env('APP_DEBUG', false) == true ? $e : '');
+		} catch (\ErrorException $e) {
+			return Json::exception('Error Exception ' . $debug = env('APP_DEBUG', false) == true ? e : '');
+		}
+	}
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function store(Request $request)
+	{
+		//
+	}
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function show($id)
+	{
+		//
+	}
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function edit($id)
+	{
+		//
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function update(Request $request, $id)
+	{
+		//
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+// Auth Module
+Route::prefix("auth")->group(function () {
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
 });
